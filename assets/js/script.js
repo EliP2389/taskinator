24e32737c2818ea955535d16ac1e046cd10dae60
Eli Pagan <mbk8689@gmail.com>
2021-10-25 15:24:40 -0400

    formEl, sumbit and event.preventDefault added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,18 @@
-var buttonEl = document.querySelector("#save-task");
+//var buttonEl = document.querySelector("#save-task");
+//finds the form element in the page and saves as formEl
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
   var createTaskHandler = function(){  
+
+    event.preventDefault();
+    console.log(event);
+    
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
   };
 
-buttonEl.addEventListener("click", createTaskHandler) ;
+//sumbit listens to a click of a button element and when a user presses Enter on their keyboard
+formEl.addEventListener("submit", createTaskHandler) ;
